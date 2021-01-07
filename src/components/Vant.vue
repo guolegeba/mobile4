9adcf46510f9be54afbb8b6e261a89ddca6059f3
锅了个巴 <2365102959@qq.com>
2021-01-07 14:20:15 +0800

    第一次测试

---
+++ src/components/Vant.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h3>xixi</h3>
+    <van-button type="primary">按钮</van-button>
+    <van-address-edit
+        :area-list="areaList"
+        show-postal
+        show-delete
+        show-set-default
+        show-search-result
+        :search-result="searchResult"
+        :area-columns-placeholder="['请选择', '请选择', '请选择']"
+        @save="onSave"
+        @delete="onDelete"
+        @change-detail="onChangeDetail"
+    />
+    
+  </div>
+</template>
+
+<script>
+import { Toast } from 'vant';
+import areaData from '../utils/areaData';
+
+export default {
+  data() {
+    return {
+      areaList:areaData,
+      searchResult: [],
+    }
+  },
+  methods: {
+    onSave() {
+      Toast('save');
+    },
+    onDelete() {
+      Toast('delete');
+    },
+    onChangeDetail(val) {
+      if (val) {
+        this.searchResult = [
+          {
+            name: '黄龙万科中心',
+            address: '杭州市西湖区',
+          },
+        ]
+      } else {
+        this.searchResult = [];
+      }
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/utils/vant.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import { Button,AddressEdit,Area } from 'vant';
+
+Vue.use(Button);
+Vue.use(AddressEdit);
+Vue.use(Area);
